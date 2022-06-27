160377aad088a38864b01deef8664a2f702cab57
jezzykubphom <jezzy_kubphom@hotmail.com>
2022-06-27 15:55:17 +0700

    finish 27-06-65

--- lib/states/edit_informaiton.dart
+++ lib/states/edit_informaiton.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'package:jezzyshopping/models/user_model.dart';
+import 'package:jezzyshopping/utility/my_dialog.dart';
+import 'package:jezzyshopping/widgets/show_button.dart';
 import 'package:jezzyshopping/widgets/show_form.dart';
 import 'package:jezzyshopping/widgets/show_icon_button.dart';
 import 'package:jezzyshopping/widgets/show_text.dart';
@@ -25,6 +30,8 @@ class _EidtInformationState extends State<EidtInformation> {
   TextEditingController nameController = TextEditingController();
   TextEditingController addressController = TextEditingController();
   TextEditingController telephoneController = TextEditingController();
+  File? file;
+  bool change = false; // false => non change all
 
   @override
   void initState() {
@@ -54,64 +61,154 @@ class _EidtInformationState extends State<EidtInformation> {
         onTap: () => FocusScope.of(context).requestFocus(FocusScopeNode()),
         child: LayoutBuilder(
             builder: (BuildContext context, BoxConstraints boxConstraints) {
-          return ListView(
+          return Stack(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              ListView(
                 children: [
-                  ShowForm(
-                    textEditingController: nameController,
-                    label: 'Name',
-                    iconData: Icons.fingerprint,
-                    changeFunc: (String string) {},
-                  ),
+                  newImage(boxConstraints),
+                  formName(),
+                  formAddress(),
+                  fromTelephone(),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ShowForm(
-                    textEditingController: addressController,
-                    label: 'Address',
-                    iconData: Icons.home_mini_outlined,
-                    changeFunc: (String string) {},
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ShowForm(
-                    textEditingController: telephoneController,
-                    label: 'Telephone',
-                    iconData: Icons.phone,
-                    changeFunc: (String string) {},
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 16),
-                    width: boxConstraints.maxWidth * 0.75,
-                    height: boxConstraints.maxWidth * 0.75,
-                    child: Stack(
-                      children: [
-                        ShowImageInternet(path: userModel!.Picture),
-                        ShowIconButton(
-                          iconData: Icons.add_a_photo,
-                          pressFunc: () {},
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+              buttonEdit(boxConstraints)
             ],
           );
         }),
       ),
+    );
+  }
+
+  Row formAddress() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowForm(
+          textEditingController: addressController,
+          label: 'Address',
+          iconData: Icons.home_mini_outlined,
+          changeFunc: (String string) {
+            change = true;
+          },
+        ),
+      ],
+    );
+  }
+
+  Positioned buttonEdit(BoxConstraints boxConstraints) {
+    return Positioned(
+      bottom: 0,
+      child: SizedBox(
+        width: boxConstraints.maxWidth,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ShowBotton(
+              label: 'Edit Information',
+              pressFunc: () {
+                if (change) {
+                  print('Have Change');
+                } else {
+                  MyDialog(context: context).normalDailog(
+                      title: 'Non Change',
+                      SubTitle:
+                          'Please Change Image , Name , Address , Telephone');
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> processTackPhoto({required ImageSource source}) async {
+    var result = await ImagePicker().pickImage(
+      source: source,
+      maxWidth: 800,
+      maxHeight: 800,
+    );
+    file = File(result!.path);
+    change = true;
+    setState(() {});
+  }
+
+  Row newImage(BoxConstraints boxConstraints) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Stack(
+          children: [
+            SizedBox(
+              width: boxConstraints.maxWidth * 0.75,
+              height: boxConstraints.maxWidth * 0.75,
+              child: file == null
+                  ? ShowImageInternet(path: userModel!.Picture)
+                  : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.file(
+                        file!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: ShowIconButton(
+                iconData: Icons.add_a_photo,
+                pressFunc: () {
+                  MyDialog(context: context).normalDailog(
+                      label: 'Camera',
+                      pressFunc: () {
+                        Navigator.pop(context);
+                        processTackPhoto(source: ImageSource.camera);
+                      },
+                      label2: 'Gallery',
+                      pressFunc2: () {
+                        Navigator.pop(context);
+                        processTackPhoto(source: ImageSource.gallery);
+                      },
+                      title: 'Take Photo',
+                      SubTitle: 'Please Take Photo by tab Camera or Gallery');
+                },
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
+  Row fromTelephone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowForm(
+          textEditingController: telephoneController,
+          label: 'Telephone',
+          iconData: Icons.phone,
+          changeFunc: (String string) {
+            change = true;
+          },
+        ),
+      ],
+    );
+  }
+
+  Row formName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ShowForm(
+          textEditingController: nameController,
+          label: 'Name',
+          iconData: Icons.fingerprint,
+          changeFunc: (String string) {
+            change = true;
+          },
+        ),
+      ],
     );
   }
 }
