bc5e2de9e8d0334851f21382f365d0418dd44d6d
jezzykubphom <jezzy_kubphom@hotmail.com>
2022-08-08 13:59:04 +0700

    Navbar Success

--- lib/states/buyer_service.dart
+++ lib/states/buyer_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jezzyshopping/bodys/my_order_buyer.dart';
 import 'package:jezzyshopping/bodys/shopping_mall_buyer.dart';
+import 'package:jezzyshopping/bodys/show_cart.dart';
 import 'package:jezzyshopping/utility/my_constant.dart';
 import 'package:jezzyshopping/widgets/show_menu.dart';
 import 'package:jezzyshopping/widgets/show_sign_out.dart';
@@ -15,33 +16,71 @@ class BuyerService extends StatefulWidget {
 
 class _BuyerServiceState extends State<BuyerService> {
   var titles = <String>[
-    'Shopping Mall',
+    'Shopping',
     'My Order',
+    'Cart',
   ];
 
   var bodys = <Widget>[
     const ShoppingMallBuyer(),
     const MyOrderBuyer(),
+    const ShowCart(),
   ];
 
+  var icons = <IconData>[
+    Icons.shopping_bag,
+    Icons.price_change,
+    Icons.shopping_cart,
+  ];
   int indexBody = 0;
+  var bottomNaviIems = <BottomNavigationBarItem>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    createBottomNavItems();
+  }
+
+  void createBottomNavItems() {
+    for (var i = 0; i < bodys.length; i++) {
+      bottomNaviIems.add(BottomNavigationBarItem(
+        label: titles[i],
+        icon: Icon(
+          icons[i],
+        ),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        foregroundColor: MyConstant.dark,
-        flexibleSpace: Container(
-          decoration: MyConstant().mainAppBar(),
-        ),
-        title: ShowText(
-          label: titles[indexBody],
-          textStyle: MyConstant().h2Style(),
-        ),
-      ),
+      appBar: newAppBar(),
       drawer: newDrawer(context),
       body: bodys[indexBody],
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNaviIems,
+        currentIndex: indexBody,
+        onTap: (Value) {
+          indexBody = Value;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  AppBar newAppBar() {
+    return AppBar(
+      centerTitle: true,
+      foregroundColor: MyConstant.dark,
+      flexibleSpace: Container(
+        decoration: MyConstant().mainAppBar(),
+      ),
+      title: ShowText(
+        label: titles[indexBody],
+        textStyle: MyConstant().h2Style(),
+      ),
     );
   }
 
@@ -55,7 +94,7 @@ class _BuyerServiceState extends State<BuyerService> {
             accountEmail: null,
           ),
           ShowMenu(
-            iconData: Icons.shopping_cart,
+            iconData: icons[0],
             title: titles[0],
             subTitle: 'ร้านค้าและสินค้า',
             pressFunc: () {
@@ -66,12 +105,23 @@ class _BuyerServiceState extends State<BuyerService> {
           ),
           Divider(color: MyConstant.dark),
           ShowMenu(
-            iconData: Icons.menu_open,
+            iconData: icons[1],
             title: titles[1],
             subTitle: 'รายการสั่งซื้อ',
             pressFunc: () {
               Navigator.pop(context);
               indexBody = 1;
+              setState(() {});
+            },
+          ),
+          Divider(color: MyConstant.dark),
+          ShowMenu(
+            iconData: icons[2],
+            title: titles[2],
+            subTitle: 'ตระกร้าสินค้า',
+            pressFunc: () {
+              Navigator.pop(context);
+              indexBody = 0;
               setState(() {});
             },
           ),
